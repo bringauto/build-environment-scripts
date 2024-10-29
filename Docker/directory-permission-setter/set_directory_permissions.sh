94909ff12377f3d1e8914f6cd33476f85c93af8d
MarioIvancik <mario.ivancik@bringauto.com>
2024-10-29 13:07:35 +0100

    added validation to directory paths

--- Docker/directory-permission-setter/set_directory_permissions.sh
+++ Docker/directory-permission-setter/set_directory_permissions.sh
@@ -2,7 +2,7 @@
 
 # Ensure the environment variable is set
 if [ -z "$DIRECTORIES_TO_SET" ]; then
-  echo "DIRECTORIES_TO_SET is not set."
+  echo "Error: DIRECTORIES_TO_SET environment variable is not set. Please provide a space-separated list of directories."
   exit 1
 fi
 
@@ -11,6 +11,16 @@ IFS=' ' read -r -a directories <<< "$DIRECTORIES_TO_SET"
 
 # Iterate over the array and apply chown
 for dir in "${directories[@]}"; do
+  # Ensure path starts with /
+  if [[ ! "$dir" =~ ^/ ]]; then
+    echo "Error: Path must be absolute: $dir"
+    exit 1
+  fi
+  # Prevent path traversal
+  if [[ "$path" =~ \.\. ]]; then
+    echo "Error: Path cannot contain ..: $dir"
+    exit 1
+  fi
   mkdir -p "$dir"
   chown -R "$UID":"$UID" "$dir"
 done
